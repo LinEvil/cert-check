1b323e352719fca67eff59ea612e680497b747b5
LinEvil <13027656+LinEvil@users.noreply.github.com>
2020-04-02 04:46:10 +0800

    Update mess
    Signed-off-by: LinEvil <13027656+LinEvil@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ var app = new Vue({
     data: function () {
         return {
             certs: [],
-            gist: 'https://gist.githubusercontent.com/fengkx/32278de43b1c11c99c62b3e87d9eb5db/raw/cert_status.json'
+            gist: 'https://gist.githubusercontent.com/fengkx/55678154ff2fc462f6de8a872eb75ac1/raw/cert_status.json'
         }
     },
     async created() {
